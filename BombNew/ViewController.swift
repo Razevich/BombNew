2e46fc75ee8f9ace16985ae603d22335c5751615
Razevich <razevich.c@gmail.com>
2016-01-22 15:23:59 -0600

    Initial Commit

---
+++ BombNew/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BombNew
+//
+//  Created by Colin Razevich on 1/22/16.
+//  Copyright © 2016 Colin Razevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
