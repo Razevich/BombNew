6bfa3f900f4320cb15baa59f1a6ac27b1e278d2d
Razevich <razevich.c@gmail.com>
2016-01-22 17:11:14 -0600

    first app

--- BombNew/ViewController.swift
+++ BombNew/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueBomb: UIImageView!
+
+    @IBOutlet weak var redBomb: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideBlueBomb(sender: AnyObject) {
+        if blueBomb.hidden == true {
+            blueBomb.hidden = false
+        }
+        else if blueBomb.hidden == false {
+            blueBomb.hidden = true
+        }
+    }
+    
+    @IBAction func hideRedBomb(sender: AnyObject) {
+        if redBomb.hidden == true {
+            redBomb.hidden = false
+        }
+        else if redBomb.hidden == false {
+            redBomb.hidden = true
+        }
+    }
+    
 
 }
 
